b60c5ab18c19a4d2ba869ed39e0d88987f4f0a7d
Christopher Mann <christophermann@web.de>
2023-08-27 17:36:56 +0200

    Add remaining tests for PokerHand

--- app/src/test/java/pokerhands/logic/PokerHandTest.java
+++ app/src/test/java/pokerhands/logic/PokerHandTest.java
@@ -2,6 +2,7 @@ package pokerhands.logic;
 
 import org.junit.jupiter.api.Test;
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static pokerhands.logic.CardSuit.*;
@@ -12,17 +13,45 @@ class PokerHandTest {
 
     @Test
     void compareTo_FirstHandTypeIsBetter_FirstHandWins() {
-        var hand = new PokerHand(TWO_PAIRS, Collections.emptyList());
-        var otherHand = new PokerHand(PAIR, Collections.emptyList());
+        var firstHand = new PokerHand(TWO_PAIRS, Collections.emptyList());
+        var secondhand = new PokerHand(PAIR, Collections.emptyList());
 
-        assertThat(hand.compareTo(otherHand)).isGreaterThan(0);
+        assertThat(firstHand).isGreaterThan(secondhand);
     }
 
     @Test
     void compareTo_SecondHandTypeIsBetter_SecondHandWins() {
-        var hand = new PokerHand(TWO_PAIRS, Collections.emptyList());
-        var otherHand = new PokerHand(FULL_HOUSE, Collections.emptyList());
+        var firstHand = new PokerHand(TWO_PAIRS, Collections.emptyList());
+        var secondHand = new PokerHand(FULL_HOUSE, Collections.emptyList());
 
-        assertThat(hand.compareTo(otherHand)).isLessThan(0);
+        assertThat(secondHand).isGreaterThan(firstHand);
+    }
+
+    @Test
+    void compareTo_SameHandTypeAndFirstHandHasHigherCard_FirstHandWins() {
+        var firstHand = new PokerHand(TWO_PAIRS, List.of(
+                new CardValueOccurrence(THREE, 2),
+                new CardValueOccurrence(TWO, 2),
+                new CardValueOccurrence(JACK, 1)));
+        var secondHand = new PokerHand(TWO_PAIRS, List.of(
+                new CardValueOccurrence(THREE, 2),
+                new CardValueOccurrence(TWO, 2),
+                new CardValueOccurrence(EIGHT, 1)));
+
+        assertThat(firstHand).isGreaterThan(secondHand);
+    }
+
+    @Test
+    void compareTo_SameHandTypeAndFirstHandHasHigherCard_SecondHandWins() {
+        var firstHand = new PokerHand(TWO_PAIRS, List.of(
+                new CardValueOccurrence(THREE, 2),
+                new CardValueOccurrence(TWO, 2),
+                new CardValueOccurrence(JACK, 1)));
+        var secondHand = new PokerHand(TWO_PAIRS, List.of(
+                new CardValueOccurrence(THREE, 2),
+                new CardValueOccurrence(TWO, 2),
+                new CardValueOccurrence(QUEEN, 1)));
+
+        assertThat(secondHand).isGreaterThan(firstHand);
     }
 }
