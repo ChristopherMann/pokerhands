eb521ff548e03532904e0df718c502865c23e2f7
Christopher Mann <christophermann@web.de>
2023-08-27 17:10:22 +0200

    Minor cleanup

--- app/src/test/java/pokerhands/logic/OccurrenceCounterTest.java
+++ app/src/test/java/pokerhands/logic/OccurrenceCounterTest.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 import static pokerhands.logic.CardSuit.*;
 import static pokerhands.logic.CardValue.*;
