748dc4e228a66ced64bba95ac1504b9ed373d099
Christopher Mann <christophermann@web.de>
2023-08-27 13:58:35 +0200

    Add tests for OccurrenceCounter

---
+++ app/src/test/java/pokerhands/logic/OccurrenceCounterTest.java
@@ -0,0 +1,53 @@
+package pokerhands.logic;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static pokerhands.logic.CardSuit.*;
+import static pokerhands.logic.CardValue.*;
+
+class OccurrenceCounterTest {
+    private final OccurrenceCounter occurrenceCounter = new OccurrenceCounter();
+
+    @Test
+    void countOccurences_DifferentNumberOfOccurrences_AreSortedByOccurence() {
+        var cards = List.of(
+            new Card(CLUBS, TWO),
+            new Card(CLUBS, THREE),
+            new Card(SPADES, TWO),
+            new Card(DIAMONDS, TWO),
+            new Card(SPADES, THREE)
+        );
+
+        var occurrences = occurrenceCounter.countOccurences(cards);
+
+        assertThat(occurrences).usingRecursiveComparison().isEqualTo(
+            List.of(new CardValueOccurrence(TWO, 3),
+                    new CardValueOccurrence(THREE, 2)
+            )
+        );
+    }
+
+    @Test
+    void countOccurences_SomeNumberOfOccurrences_AreSortedByCardValue() {
+        var cards = List.of(
+                new Card(CLUBS, TWO),
+                new Card(CLUBS, THREE),
+                new Card(SPADES, TWO),
+                new Card(DIAMONDS, KING),
+                new Card(SPADES, THREE)
+        );
+
+        var occurrences = occurrenceCounter.countOccurences(cards);
+
+        assertThat(occurrences).usingRecursiveComparison().isEqualTo(
+                List.of(new CardValueOccurrence(THREE, 2),
+                        new CardValueOccurrence(TWO, 2),
+                        new CardValueOccurrence(KING, 1)
+                )
+        );
+    }
+}
